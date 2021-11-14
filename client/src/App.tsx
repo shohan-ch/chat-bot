638abdb63ebb7009eb42489ad391be10b9433bae
xtareq <xtrinsic96@gmail.com>
2021-11-14 18:15:36 +0600

    add style

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -7,37 +7,18 @@ function App() {
 
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>Hello Vite + React!</p>
-        <p>
-          <button type="button" onClick={() => setCount((count) => count + 1)}>
-            count is: {count}
-          </button>
-        </p>
-        <p>
-          Edit <code>App.tsx</code> and save to test HMR updates.
-        </p>
-        <p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-          {' | '}
-          <a
-            className="App-link"
-            href="https://vitejs.dev/guide/features.html"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Vite Docs
-          </a>
-        </p>
-      </header>
+      <h3>Chat Application</h3>
+      <div className="chatbox">
+       <div className="sidebar">
+
+       </div>
+       <div className="content">
+         <div className="content-header"></div>
+         <div className="content-body"></div>
+         <div className="footer"></div>
+       </div>
+      </div>
+
     </div>
   )
 }
