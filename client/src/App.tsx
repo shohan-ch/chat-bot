2457d4e8bc1b64553a12a22878b136ebb1c4205b
xtareq <xtrinsic96@gmail.com>
2021-11-14 21:17:43 +0600

    scrolldowncontentbody

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,10 +1,20 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import logo from './logo.svg'
 import { AiOutlineSend } from "react-icons/ai";
+import { BsCheck2, BsCheck2All } from 'react-icons/bs'
 import './App.css'
 
 function App() {
   const [count, setCount] = useState(0)
+  
+  useEffect(()=>{
+    
+    initChat()
+  },[])
+
+  const initChat = ()=>{
+    div.scrollTop(div.prop('scrollHeight'));
+  }
 
   return (
     <div className="App">
@@ -22,7 +32,7 @@ function App() {
              <div className="avatar">
                <img src={logo} height="30" style={{borderRadius:"50%"}} alt=""  />
              </div>
-             <div className="avatar-right" style={{flex:'auto',paddingRight:'10px'}}> 
+             <div className="avatar-right" style={{flex:'auto',paddingRight:'8px'}}> 
                 <div style={{display:'flex',justifyContent:'space-between'}}>
                   <div className="title">
                     Elizabeth Olsen
@@ -32,12 +42,12 @@ function App() {
                 </div>
               </div>
                
-              <div style={{display:'flex',justifyContent:'space-between',marginTop:'10px'}}>
+              <div style={{display:'flex',justifyContent:'space-between',marginTop:'8px'}}>
                   <div className="small mgs" style={{color:'gray'}}>
                     hello how are you...
                   </div>
                   <div className="small">
-                    <span style={{backgroundColor:"green",padding:'3px 8px'}}>
+                    <span className="badge-success">
                       1
                     </span>
                 </div>
@@ -66,7 +76,7 @@ function App() {
                     what is the best way...
                   </div>
                   <div className="small">
-                    <span style={{backgroundColor:"green",padding:'3px 8px'}}>
+                    <span className="badge-success">
                       3
                     </span>
                 </div>
@@ -81,7 +91,7 @@ function App() {
        </div>
        <div className="content">
          <div className="content-header">
-         <div className="avatar">
+             <div className="avatar">
                <img src={logo} height="30" style={{borderRadius:"50%"}} alt=""  />
              </div>
              <div className="title">
@@ -93,13 +103,41 @@ function App() {
              </div>
             
          </div>
-         <div className="content-body">
+         <div className="content-body" id="contentBody">
 
            <div className="left">
-             Hello how are you doing?
+             <div className="msg-top">
+               Hello how are you
+             </div>
+
+             <div className="mgs-time"
+             style={{
+               fontSize:'10px',
+               float:'right'
+             }}
+             >
+                2.32pm
+             </div>
            </div>
-           <div className="right">
-            Hi. I'm fine how are you
+           <div className="right" >
+           <div className="msg-top" style={{minWidth:'30%'}}>
+              Hi i'm fine 
+             </div>
+            <div className="mgs-time"
+             style={{
+              
+               display:'flex',
+               alignItems:'center',
+               justifyContent:'flex-end'
+               
+             }}
+             >
+                 <span style={{fontSize:'12px', color:'grey'}}>3.32 PM</span>
+                 <BsCheck2 style={{color:'gray'}}/>
+                 <BsCheck2All style={{color:'gray'}}/>
+                 <BsCheck2All style={{color:'skyblue'}}/>
+        
+             </div>
            </div>
            <div className="left">
              yeah finen. Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eveniet molestias ullam ipsum, eius dignissimos incidunt tempora nam excepturi? Eum beatae corrupti vitae quod porro suscipit esse sequi quas? Similique, quia!
@@ -124,7 +162,7 @@ function App() {
            
          </div>
          <div className="content-footer">
-            <input type="text" className="txt" />
+            <input type="text" className="txt" placeholder="type here ..." />
             <button>
             Send <AiOutlineSend/> 
             </button>
